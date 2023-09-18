01484092c2b9ece33851f4ca040d5775edf87c03
Junpei <jun@note-Computer.local>
2023-09-18 15:21:58 +0900

    chore:cssの実装とタイムラインの表示機能の実装

--- frontend/src/components/timeline/TimeLine.jsx
+++ frontend/src/components/timeline/TimeLine.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./TimeLine.css";
 import Share from '../share/Share';
 import Post from '../post/Post';
-import { Posts } from '../../dummyData';
+// import { Posts } from '../../dummyData';
+import axios from "axios";
 
-export default function TimeLine() {
+export default function TimeLine({ username }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = username
+        ? await axios.get(`/posts/profile/${username}`)
+        : await axios.get("/posts/timeline/64eaf6a2f3793760499f18e2");
+      setPosts(response.data);
+    }
+    fetchPosts();
+  }, [username]);
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {Posts.map((post) => (
-          <Post post={post} key={post.id} />
+        {posts.map((post) => (
+          <Post post={post} key={post._id} />
         ))}
       </div>
     </div>
